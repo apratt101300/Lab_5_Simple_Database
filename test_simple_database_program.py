cc5f848526d40c90109ad68bd09ba8a94bbfaee0
Andrea <apratt1013@gmail.com>
2021-02-10 14:42:07 -0600

    comment out test functions (names only)

--- test_simple_database_program.py
+++ test_simple_database_program.py
@@ -7,33 +7,33 @@ from unittest import TestCase
 # TODO create test database
 
 """Test functions that interact with the Jugglar class/DB"""
-class Test_Database_Program(TestCase):
+# class Test_Database_Program(TestCase):
     
-    def test_add_juggler_check_exists():
+#     def test_add_juggler_check_exists():
 
 
-    def test_search_name_in_database():
+#     def test_search_name_in_database():
 
 
-    def test_search_name_not_in_database():
+#     def test_search_name_not_in_database():
 
 
-    def test_search_name_in_empty_database():
+#     def test_search_name_in_empty_database():
 
 
-    def test_search_with_empty_string():
+#     def test_search_with_empty_string():
 
 
-    def test_update_catches_check_changes_exist():
+#     def test_update_catches_check_changes_exist():
 
 
-    def update_catches_name_not_in_database():
+#     def update_catches_name_not_in_database():
 
 
-    def delete_by_name_check_deleted():
+#     def delete_by_name_check_deleted():
 
 
-    def delete_name_not_in_database():
+#     def delete_name_not_in_database():
 
 
     # TODO add additional test methods
