b71bacb87399ab5a4515c221979b92c64a6b92df
Andrea <apratt1013@gmail.com>
2021-02-09 19:15:53 -0600

    create menu functionality

--- simple_database_program.py
+++ simple_database_program.py
@@ -34,27 +34,76 @@ def delete_by_name(name_to_delete):
     Juggler.delete().where(Juggler.name == name_to_delete).execute()
 
 
-
-def main():
-    add_juggler('Andrea', 'US', 60)
-    add_juggler('Bob', 'UK', 500)
-    add_juggler('Maria', 'UK', 520)
-    add_juggler('Eric', 'Venezuala', 300)
-    add_juggler('Eric', 'Venezuala', 300)
-    add_juggler('Eric', 'Venezuala', 300)
+def quit_program():
+    print('GOODBYE.')
 
 
-    for juggler in Juggler.select():
-        print(juggler)
+def display_menu(menu):
+    while True:
+        print(menu)
+        choice = input('What would you like to do? ').upper()
+        if menu.is_valid_choice(choice):
+            return choice
+        else:
+            print('Please enter a valid menu option.')
+
+
+
     
 
-    delete_by_name('Bob')
-    delete_by_name('Eric')
-    update_catches('Andrea', 6000)
-    print('\nWhat it looks like after update')
 
-    for juggler in Juggler.select():
-        print(juggler)
+"""CODE FOR THE MENU FEATURE WAS ADAPTED FROM THE CAPSTONE READING LIST PROJECT"""
+class Menu():
+
+    def __init__(self):
+        self.menu_options = {}
+        self.functions = {}
+
+
+    def add_menu_option(self, key, text, function):
+        self.menu_options[key] = text
+        self.functions[key] = function
+
+
+    def is_valid_choice(self, choice):
+        return choice in self.menu_options
+
+
+    def get_function(self, choice):
+        return self.functions[choice]
+
+
+    def __str__(self):
+        menu_options = [f'{key}: {self.menu_options[key]}' for key in self.menu_options.keys()]
+        return '\n'.join(menu_options)
+
+
+
+
+
+
+
+def make_menu():
+    menu = Menu()
+    menu.add_menu_option('1', 'Add new juggler.', add_juggler)
+    menu.add_menu_option('2', 'Search juggler by name.', search_by_name)
+    menu.add_menu_option('3', 'Update juggler catches.', update_catches)
+    menu.add_menu_option('4', 'Delete juggler by name.', delete_by_name)
+    menu.add_menu_option('Q', 'Quit program.', quit_program)
+    return menu
+
+
+
+
+
+
+def main():
+    print('\nChainsaw Juggling Record Holders as of July 2018\n\n')
+    menu = make_menu()
+    while True:
+        user_choice = display_menu(menu)
+
+
     
 
 
