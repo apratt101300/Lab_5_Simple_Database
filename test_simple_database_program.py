056aeb6ad95041413b4f0d5478e3f0fd07c01f05
Andrea <apratt1013@gmail.com>
2021-02-09 21:50:04 -0600

    create outline for test file

---
+++ test_simple_database_program.py
@@ -0,0 +1,33 @@
+import simple_database_program
+from simple_database_program import Juggler, Menu
+import unittest
+
+
+"""Test functions that interact with the Jugglar class/DB"""
+
+def test_add_juggler_check_exists():
+
+
+def test_search_name_in_database():
+
+
+def test_search_name_not_in_database():
+
+
+def test_search_name_in_empty_database():
+
+
+def test_search_with_empty_string():
+
+
+def test_update_catches_check_changes_exist():
+
+
+def update_catches_name_not_in_database():
+
+
+def delete_by_name_check_deleted():
+
+
+def delete_name_not_in_database():
+
